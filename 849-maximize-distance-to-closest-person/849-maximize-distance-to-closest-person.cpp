c526201c96d25c4f164c2cb0785c6b8f9bb3b40b
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2022-01-16 19:41:28 +0530

    Time: 16 ms (48.56%), Space: 17.1 MB (63.67%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -1,49 +1,27 @@
 class Solution {
 public:
     int maxDistToClosest(vector<int>& seats) {
-
-        int n=seats.size();
-        int LrgstLengthOfCosequtiveZereos=0;
-        int countOfCurrZereos=0;
-        
-        for(int i=0;i<n;i++){
-            if(seats[i]==0){
-                countOfCurrZereos++;
-                LrgstLengthOfCosequtiveZereos= max(LrgstLengthOfCosequtiveZereos, countOfCurrZereos);
-            }
-            else{
-                countOfCurrZereos=0;
-            }
-        }
-        
-        int left=0;
-        countOfCurrZereos=0;
-        for(int i=0;i<n;i++){
-            if(seats[i]==0){
-                countOfCurrZereos++;
-                left=max(left, countOfCurrZereos);
-            }
-            else{
-                break;
-            }
-        }
-        
-        int right =0;
-        countOfCurrZereos=0;
-        for(int i=n-1;i>=0;i--){
-            if(seats[i]==0){
-                countOfCurrZereos++;
-                right=max(countOfCurrZereos, right);
-            }
-            else{
-                break;
-            }
-        }
-        
-        int FinalAnswer=  (LrgstLengthOfCosequtiveZereos+1)/2;
-        FinalAnswer= max(FinalAnswer, left);
-        FinalAnswer= max(FinalAnswer, right);
-        return FinalAnswer;
-    }
     
+        int prev=-1;
+        int ans=0;
+        
+        for(int i=0;i<seats.size();i++){
+        
+            if(seats[i]==1){
+                if(prev==-1){
+                    ans=i;
+                }
+                else{
+                    ans=max( (i-prev)/2, ans);
+                }
+    
+                prev=i;
+            }
+            
+        }
+        int n=seats.size()-1;
+        ans= max(ans, n- prev);
+
+        return ans;
+    }
 };
