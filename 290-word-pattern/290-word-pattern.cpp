0ed0553699dcd9b874dd4e5e6e32824e75de8881
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2022-01-17 16:36:27 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (50.50%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+
+    
+    bool wordPattern(string pattern, string s) {
+        
+        vector<string> words;
+        unordered_map<char, string>mp;
+        unordered_set<string>st;
+        
+        string item;
+        stringstream iss(s);
+        while (iss >> item)
+            words.push_back(item);
+        
+        if (pattern.size() != words.size())
+            return false;        
+        for(int i=0;i<pattern.size();i++){    
+            if(mp.find(pattern[i])!=mp.end()){
+                if(mp[pattern[i]]!=words[i]){
+                    return false;    
+                }
+            }else{
+                if(st.count(words[i])>0) return false;
+                mp[pattern[i]]=words[i];
+                st.insert(words[i]);
+            }
+        }
+        
+        return true;
+    }
+};
