e6c05665a3430534603b1de698287d634afe3d89
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2022-01-23 20:22:20 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (36.79%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
+        queue<int>q{{1,2,3,4,5,6,7,8,9}};
         vector<int> ans;
-        
-        for(int i=1;i<=8;i++){
-            int num=i;
-            for(int j=i+1;j<=9;j++){
-                num=num*10+j;
-                if(num>=low and num<=high){
-                    ans.push_back(num);   
-                }
+        while(!q.empty()){
+            int i=q.front();
+            q.pop();
+            if(i>=low and i<=high){
+                ans.push_back(i);
             }
+            if(i> high){
+                break;
+            }
+            int lastdigit=i%10;
+            if(lastdigit<9){
+                q.push(i*10 + lastdigit+1);
+            }
+            
         }
-        sort(ans.begin(),ans.end());
         return ans;
     }
 };
