032900a23c48712a768bd9547a59a7f69ec38419
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2022-01-29 01:43:06 +0530

    Time: 143 ms (35.62%), Space: 43.3 MB (74.33%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -1,89 +1,79 @@
 class WordDictionary {
-    struct node {
-        char c;
-        int end;
-        node *child[26];
+public:
+    
+    struct TrieNode{
+        TrieNode* child[26];
+        bool isend;
+        TrieNode(){
+            isend=false;
+            for(int i=0;i<26;i++){
+                child[i]=NULL;
+            }
+        }
     };
-    node *getNode(char c)
-    {
-        node *newnode=new node;
-        newnode->c = c;
-        newnode->end = 0;
-        for(int i=0;i<26;++i)
-            newnode->child[i] = NULL;
-        return newnode;
-    }
-    node *root = getNode('/');
+    TrieNode* root=new TrieNode();
     
-    void Trie_insert(string s)
-    {
-        int index;
-        int i=0;
-        node *curr = root;
-        while(s[i])
-        {
-            index = s[i]-'a';
-            if(!curr->child[index])
-                curr->child[index] = getNode(s[i]);
-            curr = curr->child[index];
-            ++i;
+    void insert(string &s){
+        TrieNode* curr= root;
+        for(int i=0;i<s.length();i++){
+            int index= s[i]-'a';
+            if(curr->child[index]==NULL){
+                curr->child[index]= new TrieNode();
+            }
+            curr= curr->child[index];
         }
-        curr->end +=1;
+        curr->isend=true;
     }
     
-    bool Trie_search(string s,node *curr,int pos,int n)
-    {        
-        if(s[pos]=='.')
-        {
-            bool res = false;
-            node *current = curr;
-            for(int i=0;i<26;++i)
-            {
-                if(pos==n-1 && curr->child[i])
-                {
-                    current=curr->child[i];
-                    res |= current->end>0?true:false;
+    bool searchTrie(string &s, TrieNode * curr, int pos, int n){
+
+        if(s[pos]=='.'){
+            bool flag=false;
+            TrieNode *temp=curr;
+            
+            /* 
+            its really necesaary for us to create this temp and increaement in 
+            temp=curr->child[i] in this manner as when you search a word say "b." and you have a word
+            say "bat" then if you increament curr->curr->child[i] it will go and search of isend true in char t 
+            and not give a false answer;
+
+            */
+            for(int i=0;i<26;i++){
+                if(pos==n-1 and curr->child[i]){
+                    temp= curr->child[i];
+                    flag |= temp->isend;
                 }
-                else if(curr->child[i] && Trie_search(s,curr->child[i],pos+1,n))
+                else if(curr->child[i] and searchTrie(s, curr->child[i], pos+1, n)){
                     return true;
+                }
             }
-            return res;
+            return flag;
         }
-        else if(curr->child[s[pos]-'a'])
-        {
-            if(pos==n-1)
-            {
-                curr = curr->child[s[pos]-'a'];
-                return curr->end>0?true:false;
+            
+        else if(curr->child[s[pos]-'a']){
+            if(pos==n-1){
+                curr=curr->child[s[pos]-'a'];
+                return curr->isend;
             }
-            return Trie_search(s,curr->child[s[pos]-'a'],pos+1,n);
+            return searchTrie(s,curr->child[s[pos]-'a'], pos+1, n);
         }
+        
         return false;
     }
-public:
-    /** Initialize your data structure here. */
+    
     WordDictionary() {
+        
     }
     
-    /** Adds a word into the data structure. */
     void addWord(string word) {
-        Trie_insert(word);
+        return insert(word);    
     }
     
-    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
     bool search(string word) {
-        return Trie_search(word,root,0,word.size());
+        return searchTrie(word, root, 0 , word.size());
     }
 };
 
-static const auto io_sync_off = []()
-{
-    // turn off sync
-    std::ios::sync_with_stdio(false);
-    // untie in/out streams
-    std::cin.tie(nullptr);
-    return nullptr;
-}();
 /**
  * Your WordDictionary object will be instantiated and called as such:
  * WordDictionary* obj = new WordDictionary();
