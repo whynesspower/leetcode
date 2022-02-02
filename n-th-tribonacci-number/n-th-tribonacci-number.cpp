e27a3286fa68c78acaedd717c6a81667aa163cfb
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2022-02-02 23:35:04 +0530

    Time: 0 ms, Memory: 6.4 MB - LeetHub

--- n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
-    int tribonacci(int n) {
-        unordered_map<int,int>mp;
+    
+    unordered_map<int,int>mp;
+    
+    int func(int n){
+        
         mp[0]=0;
         mp[1]=1;
         mp[2]=1;
-        for(int i=3;i<=n ;i++){
-            mp[i]=mp[i-1]+mp[i-2]+mp[i-3];
-        }
+        if(n<=2) return mp[n];
+        
+        if(mp.find(n)!=mp.end()) return mp[n];
+        
+        mp[n]= func(n-1)+func(n-2)+func(n-3);
+        
         return mp[n];
+    }
+    
+    
+    int tribonacci(int n) {
+        return func(n);   
     }
 };
