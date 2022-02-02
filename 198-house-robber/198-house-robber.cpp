90642cf800689da65b2e20fccd721a3eda602162
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2022-02-02 21:04:55 +0530

    Time: 7 ms (7.98%), Space: 7.9 MB (5.83%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    unordered_map<int,int>dp;
+    
+    int fun(vector<int>&nums){
+        if(nums.size()==1) return nums[0];
+        dp[0]=nums[0];
+        dp[1]=max(nums[0], nums[1]);
+        for(int i=2;i<nums.size();i++){
+            dp[i]=max(dp[i-1], dp[i-2]+nums[i]);
+        }
+        return dp[nums.size()-1];
+    }
+    
+    
+    int rob(vector<int>& nums) {
+        return fun(nums);    
+    }
+};
