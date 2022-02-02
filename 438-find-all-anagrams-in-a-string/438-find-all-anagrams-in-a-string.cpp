737366259985d20e3100f1ed982532f070f14fc6
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2022-02-02 13:16:23 +0530

    Time: 34 ms (34.95%), Space: 8.8 MB (42.26%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> pv(26,0), sv(26,0), res;
-        if(s.size() < p.size())
-           return res;
-        // fill pv, vector of counters for pattern string and sv, vector of counters for the sliding window
-        for(int i = 0; i < p.size(); ++i)
-        {
-            ++pv[p[i]-'a'];
-            ++sv[s[i]-'a'];
+        vector<int> ans;
+        unordered_map<char, int>mp;
+        for(int i=0;i<p.size();i++){
+            mp[p[i]]++;
         }
-        if(pv == sv)
-           res.push_back(0);
-
-        //here window is moving from left to right across the string. 
-        //window size is p.size(), so s.size()-p.size() moves are made 
-        for(int i = p.size(); i < s.size(); ++i) 
-        {
-             // window extends one step to the right. counter for s[i] is incremented 
-            ++sv[s[i]-'a'];
-            
-            // since we added one element to the right, 
-            // one element to the left should be discarded. 
-            //counter for s[i-p.size()] is decremented
-            --sv[s[i-p.size()]-'a']; 
-
-            // if after move to the right the anagram can be composed, 
-            // add new position of window's left point to the result 
-            if(pv == sv)  // this comparison takes O(26), i.e O(1), since both vectors are of fixed size 26. Total complexity O(n)*O(1) = O(n)
-               res.push_back(i-p.size()+1);
+        
+        int begin=0,end=0, counter=mp.size();
+        
+        while(end<s.size()){
+            char a = s[end];
+            if(mp.find(a)!=mp.end()){
+                mp[a]--;
+                if(mp[a]==0){
+                    counter--;
+                }
+            }
+            end++;
+            while(counter==0){
+                char b= s[begin];
+                if(mp.find(b)!=mp.end()){
+                    mp[b]++;
+                    if(mp[b]>0) counter++;
+                }
+                if(end-begin==p.size()){
+                    ans.push_back(begin);
+                }
+                begin++;
+            }
         }
-        return res;
+        
+        return ans;
     }
 };
