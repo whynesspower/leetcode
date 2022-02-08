939bfa90d6c88f5274e0256dab1d918d33845fd9
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2022-02-09 03:13:00 +0530

    Time: 4 ms (43.40%), Space: 6 MB (25.31%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
-    int addDigits(int num) {
-        if(num==0)return 0;
-        if(num%9==0) return 9;
-        else{
-            return num%9;
-        }
+    int addDigits(int n) {
+        if(n==0)return 0;
+        if(n%9==0)return 9;
+        else return n%9;
     }
 };
