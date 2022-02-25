317b93f8f159eae6054a2a8ac12bb9daa2788c3d
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2022-02-25 12:45:39 +0530

    Time: 3 ms (31.35%), Space: 6.2 MB (35.61%) - LeetHub

---
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        
+        istringstream v1(version1);
+        istringstream v2(version2);
+        
+        string num1, num2;
+        
+        while(!v1.eof() or !v2.eof()){
+            if(!v1.eof()){
+                getline(v1, num1, '.');
+            }    
+            else{
+                num1="0";
+            }
+            
+            
+            if(!v2.eof()){
+                getline(v2,num2, '.');
+            }else{
+                num2="0";
+            }
+        
+            
+            int n1=stoi(num1);
+            int n2=stoi(num2);
+            
+            if(n1==n2)continue;
+            return (n1>n2)?1:-1;
+            
+        }
+        
+        return 0;
+        
+        
+        
+    }
+};
