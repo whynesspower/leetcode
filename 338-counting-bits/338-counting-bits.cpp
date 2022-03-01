2d9fc730644485bf0e2621998babe29bd7c0e373
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2022-03-01 06:41:08 +0530

    Time: 9 ms (40.84%), Space: 7.7 MB (89.06%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
-    
+
     vector<int> countBits(int n) {
         vector<int>ans(n+1);
         for(int i=0;i<=n;i++){
+            FillArray(ans, i);
+        }
+        return ans;
+    }
+    
+    void FillArray(vector<int>&ans, int i){
             int count=0;
             int j=i;
             while(j!=0){
@@ -11,7 +17,5 @@ public:
                 count++;
             }
             ans[i]=count;
-        }
-        return ans;
     }
 };
