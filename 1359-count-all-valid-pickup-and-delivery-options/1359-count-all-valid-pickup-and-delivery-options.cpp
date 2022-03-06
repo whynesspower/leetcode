2ac005b2b863f69ee03118d4d9ff29817b6b3423
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2022-03-06 14:13:03 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (82.70%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countOrders(int n) {
+        long ans=1;
+        int mod = 1e9+7;
+        for(int i=1;i<=n;i++){
+            ans=ans*(2*i-1)*i %mod;
+        }
+        return ans;
+    }
+};
