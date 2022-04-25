d1320ac7960d1732b3175e9306f30cb50a43539d
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2022-04-25 19:31:51 +0530

    Time: 234 ms (38.15%), Space: 62.9 MB (5.08%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.cpp
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -1,41 +1,53 @@
 class UndergroundSystem {
+    
 public:
-    //startStation:endStation -> {totalTime, numberOfTrips}
-    unordered_map<string, pair<long long, int>> stationTimes;
-    //customerId -> startStation
-    unordered_map<int, pair<string,int>> inTransit;
-    UndergroundSystem() {
-        stationTimes.clear();
-        inTransit.clear();
+    
+    unordered_map<int, pair<string, int>> m1;
+    unordered_map<string, vector<long long>>m2;
+    UndergroundSystem(){
+        m1.clear();
+        m2.clear();
     }
     
     void checkIn(int id, string stationName, int t) {
-        if(inTransit.find(id) != inTransit.end()) return;
-        inTransit[id] = {stationName, t};
+        if(m1.find(id)!=m1.end()) return;
+        pair<string, int>p={stationName, t};
+        m1[id]=p;
     }
     
     void checkOut(int id, string stationName, int t) {
-        auto& info = inTransit[id];
-        string startStation = info.first;
-        int startTime = info.second;
-        string key = startStation + ":" + stationName;
-        int time = t - startTime;
-        
-        if(stationTimes.find(key) != stationTimes.end()) {
-            auto& oldTimes = stationTimes[key];
-            oldTimes.first += time;
-            oldTimes.second++;
-        } else {
-            stationTimes[key] = {time, 1};
+        auto p1= m1[id];
+        int t2= t-p1.second;
+        string aad=p1.first+ ":"+ stationName;
+        // pair<string,string>p={p1.first, stationName};
+        if(m2.find(aad)!=m2.end()){
+            m2[aad].push_back(t2);
         }
-        inTransit.erase(id);
-        
+        else{
+            vector<long long >temp;
+            temp.push_back(t2);
+            m2[aad]=temp;
+        }         
+        m1.erase(id);
     }
     
     double getAverageTime(string startStation, string endStation) {
-        string key = startStation + ":" + endStation;
-        auto& info = stationTimes[key];
-        double avg = (double)info.first / (double)info.second;
-        return avg;
+        // pair<string, string>p={startStation, endStation};
+        string aad=startStation+ ":"+ endStation;
+        vector<long long>temp=m2[aad];
+        int n= temp.size();
+        double sum=0.0;
+        for(int i=0;i<n;i++){
+            sum+=temp[i];
+        }
+        return (double)sum/(double)n;
     }
 };
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem* obj = new UndergroundSystem();
+ * obj->checkIn(id,stationName,t);
+ * obj->checkOut(id,stationName,t);
+ * double param_3 = obj->getAverageTime(startStation,endStation);
+ */
