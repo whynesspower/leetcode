b9fd0e7d4fe5d2bff2f5b8bdff75d70af4b639b9
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2022-05-01 00:31:44 +0530

    Time: 5 ms (30.48%), Space: 8.3 MB (52.62%) - LeetHub

---
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void dfs(string s, string d,map<string, vector<pair<string, double>>>&mp, set<string>&v, double &ans, double temp  ){
+        if(v.find(s)!=v.end()) return;
+        else{
+            v.insert(s);
+            if(s==d){
+                ans=temp;
+                return  ;
+            }
+            else{
+                
+                for(auto a:mp[s]){
+                    dfs(a.first, d, mp,v, ans,temp*a.second );
+                }
+            }
+        }
+    }
+    
+    
+    vector<double> calcEquation(vector<vector<string>>& e, vector<double>& v, vector<vector<string>>& q) {
+        map<string, vector<pair<string, double>>>mp;
+        for(int i=0;i<e.size();i++){
+            mp[e[i][0]].push_back({e[i][1], v[i]});
+            mp[e[i][1]].push_back({e[i][0], 1/v[i]});
+        }
+        
+        vector<double>res;
+        for(int i=0;i<q.size();i++){
+            string s=q[i][0];
+            string d=q[i][1];
+            set<string>visited;
+            double ans=-1.0;
+            if(mp.find(s)!=mp.end()) 
+            dfs(s, d, mp, visited,ans, 1.0 );
+            res.push_back(ans);
+        }
+        return res;
+    }
+};
