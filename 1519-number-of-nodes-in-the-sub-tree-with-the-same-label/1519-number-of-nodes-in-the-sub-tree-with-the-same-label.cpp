7e8c99076ed778bd489386b2236241e50a4f7466
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-01-12 16:14:05 +0530

    Time: 694 ms (73.33%), Space: 228.8 MB (31.85%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int>ans;
+    vector<int>dfs(vector<vector<int>>&adj, string &s, int node, vector<bool>&vis){
+        vis[node]=true;
+        vector<int>temp(26,0);
+        for(auto x: adj[node]){
+            if(vis[x]==false and ans[node]==-1){
+                vector<int>t=dfs(adj, s, x, vis);
+                for(int i=0;i<26;i++){
+                    temp[i]+=t[i];
+                }
+            }
+        }
+        temp[s[node]-'a']++;
+        int a=temp[s[node]-'a'];
+        ans[node]=a;
+        return temp;
+    }
+    
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        vector<vector<int>>adj(n+1);
+        for(auto x:edges){
+            int a= x[0];
+            int b= x[1];
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+        vector<bool>vis(n, false);
+        ans.resize(n, -1);
+        dfs(adj, labels, 0, vis);
+        return ans;
+    }
+};
