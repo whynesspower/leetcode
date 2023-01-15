6a3edbc21b40d1f093bb0cb61c939cac70d5b135
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-01-15 15:03:51 +0530

    Time: 848 ms (100.00%), Space: 281.1 MB (50.00%) - LeetHub

--- 2538-difference-between-maximum-and-minimum-price-sum/2538-difference-between-maximum-and-minimum-price-sum.cpp
+++ 2538-difference-between-maximum-and-minimum-price-sum/2538-difference-between-maximum-and-minimum-price-sum.cpp
@@ -2,6 +2,9 @@
 class Solution {
 public:
     vector<unordered_map<int, ll>>dp;
+    
+    // dp[i][par] will give the maximum path sum possible if we start from
+    // node "i" and that node has a parent of "par"
     long long dfs(vector<long long>adj[], ll src,  vector<int>& price, ll par){
         if(dp[src].count(par)){
             return dp[src][par];
@@ -27,6 +30,9 @@ public:
             adj[a].push_back(b);
             adj[b].push_back(a);
         }
+        // dfs(i, parent) will tell the maximum sum of the path such
+        // that if we start from node "I", which has node "par" as parent,
+        // what will the maximum path sum we will have.  
         ll ans=0;
         for(ll i=0;i<n;i++){
             if (size(adj[i]) == 1){
