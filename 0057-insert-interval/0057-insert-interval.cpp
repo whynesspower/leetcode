8d7986a8576d7b1c5e639a934a9a39f00af40287
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-01-16 12:03:49 +0530

    Time: 16 ms (83.48%), Space: 17.1 MB (45.20%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,58 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> ans;
-        int n=intervals.size();
-        int start_indx=0, end_indx=intervals.size()-1;
-        if(intervals.size()==0){
-            ans.push_back(newInterval);
-            return ans;
+   vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n = intervals.size(), i = 0;
+        vector<vector<int>> res;
+        //case 1: no overlapping case before the merge intervals
+		//compare ending point of intervals to starting point of newInterval
+        while(i < n && intervals[i][1] < newInterval[0]){
+            res.push_back(intervals[i]);
+            i++;
+        }                           
+		//case 2: overlapping case and merging of intervals
+        while(i < n && newInterval[1] >= intervals[i][0]){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
         }
-        int low=0, high=intervals.size()-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(intervals[mid][0]<=newInterval[0]){
-                // high=mid-1;
-                start_indx=mid;
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+        res.push_back(newInterval);
+        // case 3: no overlapping of intervals after newinterval being merged
+        while(i < n){
+            res.push_back(intervals[i]);
+            i++;
         }
-        low=0, high=intervals.size()-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(intervals[mid][1]>=newInterval[1]){
-                end_indx=mid;
-                // low=mid+1;
-                high=mid-1;
-            }
-            else{
-                // high=mid-1;
-                low=mid+1;
-            }
-        }
-        if(intervals[start_indx][1]<newInterval[0]){
-            start_indx++;
-        }
-        if(intervals[end_indx][0]>newInterval[1]){
-            end_indx--;
-        }
-        for(int i=0;i<start_indx;i++){
-            ans.push_back(intervals[i]);
-        }
-            vector<int>temp;
-            int a= newInterval[0];
-            if(start_indx<n) a=min(intervals[start_indx][0],newInterval[0]); 
-            temp.push_back(a);
-            int b= newInterval[1];
-            if(end_indx>=0) b= max(intervals[end_indx][1], newInterval[1]);
-            temp.push_back(b);
-            ans.push_back(temp);
-        for(int i=end_indx+1;i<intervals.size();i++){
-            ans.push_back(intervals[i]);
-        }
-        return ans;
+        return res;
     }
 };
