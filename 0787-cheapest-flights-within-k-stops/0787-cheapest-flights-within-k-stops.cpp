1163787a5a64e148d247be2d35fa7a5cf1f3fabd
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-01-26 17:00:44 +0530

    Time: 24 ms (82.98%), Space: 13 MB (85.13%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,17 +1,39 @@
-class Solution {
+
+class Solution
+{
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<int>>dp(k+2, vector<int>(n, INT_MAX));
-        for(int i=0;i<=k+1;i++){
-            dp[i][src]=0;
+    int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k)
+    {
+        vector<pair<int, int>> adj[n];
+        // destrination, cost
+
+        for (auto &it : flights)
+        {
+            adj[it[0]].push_back({it[1], it[2]});
         }
-        for(int i=1;i<=(k+1); i++){
-            for(auto &f:flights){
-                if(dp[i-1][f[0]]!=INT_MAX){
-                    dp[i][f[1]]=min(dp[i][f[1]], dp[i-1][f[0]]+f[2]);
-                }    
+        vector<int> dist(n, 1e9);
+        dist[src] = 0;
+        queue<pair<int, pair<int, int>>> q;
+        q.push({0, {src, 0}});
+        // stops, node, cost
+        while (!q.empty())
+        {
+            auto it = q.front();
+            q.pop();
+            int stops = it.first;
+            int node = it.second.first;
+            int cost = it.second.second;
+            if (stops > k)
+                continue;
+            for (auto itr : adj[node])
+            {
+                if (dist[itr.first] > (cost + itr.second) and stops <= k)
+                {
+                    dist[itr.first] = cost + itr.second;
+                    q.push({stops + 1, {itr.first, cost + itr.second}});
+                }
             }
         }
-        return dp[k+1][dst]==INT_MAX?-1:dp[k+1][dst];
+        return dist[dst] == 1e9 ? -1 : dist[dst];
     }
 };
