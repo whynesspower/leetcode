e71c9ede228efa1696532cc4d4ae87f288e3cef1
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-02-24 19:34:28 +0530

    Time: 349 ms (85.59%), Space: 89.1 MB (37.29%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    
     int minimumDeviation(vector<int>& nums) {
-        int mini=INT_MAX; 
-        priority_queue<int>pq;
-        
-        for(int i=0;i<nums.size();i++){
-             
-             if(nums[i]%2!=0){
-                 nums[i]*=2;
-             }
+        set <int>  s;
+        for(int i = 0; i<nums.size() ; ++i)
+        {
+            if(nums[i] % 2 == 0)
+                s.insert(nums[i]);
+            
+            else
+                s.insert(nums[i] * 2);
         }
         
-        for(int i=0;i<nums.size();i++){
-            mini=min(mini, nums[i]);
-            pq.push(nums[i]);
+        int diff = *s.rbegin() - *s.begin();
+        
+        while(*s.rbegin() % 2 == 0)
+        {
+            int x = *s.rbegin();
+            s.erase(x);
+            s.insert(x/2);
+            diff = min(diff, *s.rbegin() - *s.begin());
         }
-        
-        int ans=INT_MAX;
-        
-        while(pq.top()%2==0){
-            int tep=pq.top();
-            pq.pop();
-            ans=min(ans, tep-mini);
-            mini=min(mini, tep/2);
-            tep/=2;
-            pq.push(tep);
-        }
-        
-        return min(ans, pq.top()-mini);
+        return diff;
     }
 };
