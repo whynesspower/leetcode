189b2148a5b0b3836163a445f7bcaac473d4c536
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-04-20 01:48:35 +0530

    Time: 574 ms (5.08%), Space: 190.8 MB (5.43%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
-    int maxStep = 0;
-    int longestZigZag(TreeNode* root) {
-        dfs(root, true, 0);
-        dfs(root, false, 0);
-        return maxStep;
-    }
-    void dfs(TreeNode* root, bool isLeft, int step) {
-        if (!root) return;
-        maxStep = max(maxStep, step); // update max step sofar
-        if (isLeft) {
-            dfs(root->left, false, step + 1); // keep going from root to left
-            dfs(root->right, true, 1); // restart going from root to right
-        } else {
-            dfs(root->right, true, step + 1); // keep going from root to right
-            dfs(root->left, false, 1); // restart going from root to left
+    map<pair<TreeNode*,int>,int> m;
+    int count(TreeNode* root,int dir){
+        if(!root){
+            return 0;
         }
+        if(m[{root,dir}]!=0){
+            return m[{root,dir}];
+        }
+        int c = 0;
+        if(dir){
+            c = 1 + count(root->right,0);
+        }
+        else{
+            c = 1 + count(root->left,1);
+        }
+        return m[{root,dir}] = c;
+    }
+    void solve(TreeNode* root,int &ans){
+        if(!root){
+            return;
+        }
+        int countRight = count(root,1);
+        int countLeft = count(root,0);
+        ans = max(ans,max(countRight,countLeft));
+        solve(root->left,ans);
+        solve(root->right,ans);
+    }
+    int longestZigZag(TreeNode* root){
+       int ans = 0;
+       solve(root,ans);
+       return ans-1;
     }
 };
