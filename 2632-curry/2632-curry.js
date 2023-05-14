c6a19338af6f60e961bee6a5c5c671cf4caedb57
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-05-14 18:37:52 +0530

    Time: 100 ms (5.38%), Space: 48.6 MB (33.26%) - LeetHub

---
+++ 2632-curry/2632-curry.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var curry = function(fn) {
+    return function curried(...args) {
+        
+        if(args.length<fn.length){
+            return function(...args2){
+                return curried(...args, ...args2);
+            }
+        }
+        else{
+            return fn(...args);
+        }
+        
+    };
+};
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
