6358e8b09f15afae5452497ee5f10af5ab901de7
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-06-02 23:14:37 +0530

    Time: 69 ms (12.60%), Space: 44.2 MB (43.25%) - LeetHub

---
+++ 2695-array-wrapper/2695-array-wrapper.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ */
+var ArrayWrapper = function(nums) {
+    this.nums=nums;
+};
+
+ArrayWrapper.prototype.valueOf = function() {
+    var sum=0;
+    for( var x of this.nums){
+        sum+=x;
+    }
+    return sum;
+}
+
+ArrayWrapper.prototype.toString = function() {
+    return "["+ this.nums.join(",")+"]";
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
