4a79d03b245c83f41b2e563b87ece3d724d6ee05
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-06-21 10:45:34 +0530

    Time: 144 ms (16.79%), Space: 38.5 MB (71.17%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -1,45 +1,23 @@
 class Solution {
 public:
-    long long minCost(vector<int>& nums, vector<int>& cost) {
-        long long n=nums.size();
-        vector<long long>s1(n);
-        vector<long long>s2(n);
-        vector<long long>p1(n);
-        vector<long long>p2(n);
-        vector<pair<long long,long long>>vec;
-        for(long long i=0;i<n;i++){
-            vec.push_back({nums[i], cost[i]});
+ long long minCost(vector<int>& A, vector<int>& cost) {
+        long long l = 1, r = 1000000, res = f(A, cost, 1), x;
+        while (l < r) {
+            x = (l + r) / 2;
+            long long y1 = f(A, cost, x), y2 = f(A, cost, x + 1);
+            res = min(y1, y2);
+            if (y1 < y2)
+                r = x;
+            else
+                l = x + 1;
         }
-        sort(begin(vec),end(vec));
-        p1[0]=vec[0].second;
-        p2[0]=vec[0].second*vec[0].first;
-        s1[n-1]=vec[n-1].second;
-        s2[n-1]=vec[n-1].second*vec[n-1].first;
-        for(long long i=1;i<n;i++){
-            p1[i]=p1[i-1];
-            p2[i]=p2[i-1];
-            p1[i]+=vec[i].second;
-            p2[i]+=(vec[i].second*vec[i].first);
-        }
+        return res;
+    }
 
-        for(long long i=n-2;i>=0;i--){
-            s1[i]=s1[i+1];
-            s2[i]=s2[i+1];
-            s1[i]+=(vec[i].second);
-            s2[i]+=(vec[i].first*vec[i].second);
-        }
-
-        long long ans=LONG_MAX;
-        for(long long i=0;i<n;i++){
-            long long currcost=0;
-            if(i>0){
-                currcost+= (vec[i].first*(p1[i-1])-p2[i-1]);
-            }
-            if(i<n-1){
-                currcost+= (s2[i+1]-vec[i].first*(s1[i+1]) );
-            }
-            ans=min(ans, currcost);
-        }
-        return ans;
+    long long f(vector<int>& A, vector<int>& cost, int x) {
+        long long res = 0;
+        for (int i = 0; i < A.size(); ++i)
+            res += 1L * abs(A[i] - x) * cost[i];
+        return res;
     }
 };
