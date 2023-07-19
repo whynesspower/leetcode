44d1335681a3d5c74ee3e0e93df451515fda2b0b
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-07-19 14:47:29 +0530

    Time: 530 ms (30.37%), Space: 89.7 MB (94.24%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& v) {
+        sort(v.begin(), v.end());
+        int i=0,j=0;
+        int prevIndx=0, currIndx=1;
+        int n=v.size();
+        int cntOfRemovals=0;
+        if(n<=1) return cntOfRemovals;
+        while(currIndx<n){
+            int x1=v[prevIndx][0];
+            int y1=v[prevIndx][1];
+            int x2=v[currIndx][0];
+            int y2=v[currIndx][1];
+            if(y1<=x2){
+                prevIndx=currIndx;
+                currIndx++;
+                continue;
+            }
+            else{
+                // collition occurs
+                if(y1<=y2){
+                    // prevIndx is smaller range, hence gredily chose this
+                    cntOfRemovals++;
+                    currIndx++;
+                }
+                else{
+                    prevIndx=currIndx;
+                    currIndx++;
+                    cntOfRemovals++;
+                }
+            }
+        }
+        return cntOfRemovals;
+    }
+};
