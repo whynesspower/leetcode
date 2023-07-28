64954b9917a07bf93033f57672a7e8fadd4cb177
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-07-28 17:20:19 +0530

    Time: 4 ms (63.90%), Space: 7.6 MB (43.52%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     vector<vector<int>>dp;
     int solve(int i, int j,vector<int>&nums){
+        // returns the max score player 1 
+        // can acheive in between the index i and j
         if(i>j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
         int x = nums[i]+ min(solve(i+1, j-1, nums), solve(i+2, j, nums));
@@ -13,6 +15,8 @@ public:
     bool PredictTheWinner(vector<int>& nums) {
         int n=nums.size();
         dp.resize(n, vector<int>(n, -1));
+        // dp[i][j] will the the max score of 
+        // player 1 between the index i, j
         int a= solve(0, n-1, nums);
         int b=0;
         for(auto &x:nums){
