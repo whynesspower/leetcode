80581f57da15716d2fdb01be04efd4ca8f5defa2
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-07-31 15:47:17 +0530

    Time: 32 ms (85.55%), Space: 15.3 MB (41.16%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -5,8 +5,16 @@ public:
     int n = a.size();
     int m = b.size();
     vector<vector<int>> dp(n+1 , vector<int> (m+1));
-
-    for(int i=1 ; i<=n ; i++)                 // base case filling up
+    // there are two base case
+    // 1. when one of the two strings is of length 0
+    // 2. when we have finished taking characters from one of the strings, then the
+       // remaining part of the second string needs to be deleted fully 
+       // initializing dp with 0 by deault takes cares of the 1st case. 
+       
+       
+       
+    // second base case filling up
+    for(int i=1 ; i<=n ; i++)       
         dp[i][0] = dp[i-1][0] + a[i-1];
 
     for(int j=1; j<=m ; j++)                 // base case filling up
