8b0b0ba700d32afb15cb102090569bb30db80768
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-08-02 18:26:46 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (20.03%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    vector<vector<int>> permute(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        helper(nums,0, nums.size());
+        return ans;
+    }
+
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        helper(nums,0, nums.size());
+        return ans;
+    }
+    void helper(vector<int>nums,int i, int j){
+        if(i==j-1){
+            ans.push_back(nums);
+            return;   
+            
+        }
+        for(int k=i;k<j;k++){
+            // if(i!=k and nums[i]==nums[k])continue;
+            swap(nums[i], nums[k]);
+            helper(nums, i+1, j);
+        }
+    }
+};
