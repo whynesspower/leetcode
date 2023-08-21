931d6a2a8b9811fd2a11c02166c0b14b1aa19a87
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-08-21 21:58:05 +0530

    Time: 20 ms (60.60%), Space: 13.5 MB (57.60%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        
+        int size = s.size();
+        
+        string postfix = s.substr(1, size-1);
+        string prefix = s.substr(0, size-1);
+        
+        string sFold = postfix + prefix;
+        
+        return sFold.find(s) != string::npos;
+    }
+};
