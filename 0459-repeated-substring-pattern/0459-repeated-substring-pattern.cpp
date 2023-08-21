91f5ad8336f7ab1ada77770912de6b1aeb36668f
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-08-22 02:35:03 +0530

    Time: 12 ms (92.71%), Space: 12.6 MB (76.58%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
+    vector<int>lps;
+    void createLps(string &s){
+        int i=1;
+        int j=0;
+        while(i<s.size()){
+            if(s[i]==s[j]){
+                lps[i]=j+1;
+                i++;
+                j++;
+            }
+            else{
+                if(j==0){
+                    lps[i]=0;
+                    i++;
+                }
+                else{
+                    j=lps[j-1];
+                }
+            }
+        }
+    }
     bool repeatedSubstringPattern(string s) {
-        
-        int size = s.size();
-        
-        string postfix = s.substr(1, size-1);
-        string prefix = s.substr(0, size-1);
-        
-        string sFold = postfix + prefix;
-        
-        return sFold.find(s) != string::npos;
+        int n=s.size();
+        lps.resize(n, 0);
+        createLps(s);
+        int m=lps[n-1];
+        int l=n-m;
+        if(m==0) return false;
+        return m%l==0;
     }
 };
