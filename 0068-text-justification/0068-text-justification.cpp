773c8f73f41e5b2915c9e3fe917f603506c13713
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-08-25 04:11:34 +0530

    Time: 4 ms (33.49%), Space: 7.4 MB (69.88%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.cpp
@@ -0,0 +1,87 @@
+class Solution {
+public:
+    // int i=-1, j;
+    string fillString(int &i, int j, vector<string>&words, int maxWidth, int currCharCnt){
+        string currStr="";
+        int whiteSpaces= maxWidth-currCharCnt;
+        
+        if(j==words.size()-1){
+            while(i<j){
+                currStr+=words[i];
+                currStr+=' ';
+                i++;
+                whiteSpaces--;
+            }
+            currStr+=words[i];
+            i++;
+            while(whiteSpaces--){
+                currStr+=' ';
+            }
+        }
+        else{
+            int numBlanks= j-i;
+            if(numBlanks==0){
+                currStr+=words[i];
+                while(whiteSpaces--){
+                    currStr+=' ';
+                }
+                i++;
+            }
+            else{
+
+                int extraWhiteSpace=whiteSpaces%numBlanks;
+                int regularWhiteSpace=whiteSpaces/numBlanks;
+                string extraWhiteStr="";
+                while(regularWhiteSpace){
+                    extraWhiteStr+=' ';
+                    regularWhiteSpace--;
+                }
+                while(i<=j){
+                    currStr+=words[i];
+                    if(i<j)currStr+=extraWhiteStr;
+                    if(extraWhiteSpace){
+                        currStr+=' ';
+                        extraWhiteSpace--;
+                    }
+                    i++;
+                }
+            }
+        }
+        // cout<<i<<" "<<j<<" "<<endl;
+        currCharCnt=0;
+        return currStr;
+    }
+    
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        vector<string> ans;
+        int  j=0,i=0;
+        int n=words.size();
+        int currCharCnt=0;
+        int tempHit=0;
+        while(j<n){
+            
+            // cout<<currCharCnt<<" BEFORE: Curr CHar Cnt"<<endl;
+            currCharCnt+=words[j].length();
+            if(currCharCnt+j-i>maxWidth){
+                string currString=fillString(i, j-1, words, maxWidth, currCharCnt-words[j].length());
+                ans.push_back(currString);
+                currCharCnt=words[j].length();
+                if(currCharCnt>maxWidth){
+                    
+                     tempHit++;
+                    string currString=fillString(i, j, words, maxWidth, currCharCnt);
+                    ans.push_back(currString);
+                    currCharCnt=0;
+                }
+            }
+            if(j==n-1){
+                string currString=fillString(i, j, words, maxWidth, currCharCnt);
+                ans.push_back(currString);
+            }
+            j++;
+        }
+        cout<<tempHit<<" dfdfdsfd: Curr CHar Cnt"<<endl;
+        
+        return ans;
+    }
+};
