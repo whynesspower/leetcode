a40f4ebd8aba7356d132608d27d1be9fc7033906
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-08-25 04:19:17 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (5.93%) - LeetHub

--- 0068-text-justification/0068-text-justification.cpp
+++ 0068-text-justification/0068-text-justification.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    // int i=-1, j;
     string fillString(int &i, int j, vector<string>&words, int maxWidth, int currCharCnt){
         string currStr="";
         int whiteSpaces= maxWidth-currCharCnt;
-        
         int numBlanks= j-i;
+        
         if(j==words.size()-1 || numBlanks==0){
             while(i<j){
                 currStr+=words[i];
@@ -20,27 +19,24 @@ public:
             }
         }
         else{
-        
-
-                int extraWhiteSpace=whiteSpaces%numBlanks;
-                int regularWhiteSpace=whiteSpaces/numBlanks;
-                string extraWhiteStr="";
-                while(regularWhiteSpace){
-                    extraWhiteStr+=' ';
-                    regularWhiteSpace--;
+            int extraWhiteSpace=whiteSpaces%numBlanks;
+            int regularWhiteSpace=whiteSpaces/numBlanks;
+            string extraWhiteStr="";
+            while(regularWhiteSpace){
+                extraWhiteStr+=' ';
+                regularWhiteSpace--;
+            }
+            while(i<=j){
+                currStr+=words[i];
+                if(i<j)currStr+=extraWhiteStr;
+                if(extraWhiteSpace){
+                    currStr+=' ';
+                    extraWhiteSpace--;
                 }
-                while(i<=j){
-                    currStr+=words[i];
-                    if(i<j)currStr+=extraWhiteStr;
-                    if(extraWhiteSpace){
-                        currStr+=' ';
-                        extraWhiteSpace--;
-                    }
-                    i++;
-                }
+                i++;
+            }
             
         }
-        // cout<<i<<" "<<j<<" "<<endl;
         currCharCnt=0;
         return currStr;
     }
@@ -50,10 +46,8 @@ public:
         int  j=0,i=0;
         int n=words.size();
         int currCharCnt=0;
-        int tempHit=0;
         while(j<n){
             
-            // cout<<currCharCnt<<" BEFORE: Curr CHar Cnt"<<endl;
             currCharCnt+=words[j].length();
             if(currCharCnt+j-i>maxWidth){
                 string currString=fillString(i, j-1, words, maxWidth, currCharCnt-words[j].length());
@@ -66,8 +60,6 @@ public:
             }
             j++;
         }
-        cout<<tempHit<<" dfdfdsfd: Curr CHar Cnt"<<endl;
-        
         return ans;
     }
 };
