f219a1563b919c9a6aa921ec33e6323e7f83bfa4
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-08-29 13:49:59 +0530

    Time: 41 ms (32.43%), Space: 18.9 MB (26.64%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int bestClosingTime(string c) {
-        
         int n=c.size();
         vector<int>cntY(n+1, 0);
         vector<int>cntN(n+1, 0);
@@ -9,23 +8,22 @@ public:
         for(int i=n-1;i>=0;i--){
             cntY[i]=cntY[i+1]+( c[i]=='Y'?1:0);
         }
-        
+
         cntN[0]=(c[0]=='N'?1:0);
+
         for(int i=1;i<n;i++){
             cntN[i]=cntN[i-1]+(c[i]=='N'?1:0);
-            
         }
         cntN[n]=cntN[n-1];
-        
         int ans=INT_MAX;
         int ansIndex=0;
-        
         for(int i=0;i<=n;i++){
             if(i==0){
                 if(ans>cntY[0]){
                     ans=min(ans, cntY[0]);
                     ansIndex=i;    
                 }
+                
             }
             else{
                 if(ans>cntY[i]+cntN[i-1]){
@@ -33,6 +31,7 @@ public:
                     ansIndex=i;    
                 }
             }
+           
         }
         return ansIndex;
     }
