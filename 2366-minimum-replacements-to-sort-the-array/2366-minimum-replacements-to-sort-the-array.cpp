95976fbf2702ab65ab3d86cc5eeafe6ed91d4b0d
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-08-31 03:08:16 +0530

    Time: 85 ms (89.33%), Space: 54.6 MB (87.80%) - LeetHub

---
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long minimumReplacement(std::vector<int>& nums) {
+        long long operations = 0;
+        long long prev_bound = nums.back();
+        
+        for (auto it = nums.rbegin() + 1; it != nums.rend(); ++it) {
+            long long num = *it;
+            long long no_of_times = (num + prev_bound - 1) / prev_bound;
+            operations += no_of_times - 1;
+            prev_bound = num / no_of_times;
+        }
+        
+        return operations;
+    }
+};
