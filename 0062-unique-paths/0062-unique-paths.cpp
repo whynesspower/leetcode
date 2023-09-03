8fd2ac4965a022aae12c0ba6e6495b8ab4520b98
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-09-03 11:25:29 +0530

    Time: 4 ms (14.96%), Space: 6.5 MB (24.88%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int count(vector<vector<int>>&dp, int i, int j){
+        if(i==0 and j==0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int ways=((i-1)>=0?count(dp, i-1, j):0 + (j-1)>=0?count(dp, i, j-1):0);
+        dp[i][j]=ways;
+        return ways;
+    }
+    
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m , vector<int>(n, -1)); 
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 and j==0)continue;
+                dp[i][j]=(((i-1)>=0)?dp[i-1][j]:0)+(((j-1)>=0)?dp[i][j-1]:0);
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                // dp[i][j]=i-1>=0?dp[i-1][j]:0+(j-1)>=0?dp[i][j-1]:0;
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        // return count(dp, n-1, m-1);
+        return dp[m-1][n-1];
+    }
+};
