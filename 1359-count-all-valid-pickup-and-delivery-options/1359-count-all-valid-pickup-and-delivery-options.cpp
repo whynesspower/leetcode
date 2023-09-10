c648282ed5747e50f20f0f2ef2339074b71d3976
Yashraj Shukla <77494053+whynesspower@users.noreply.github.com>
2023-09-10 23:44:24 +0530

    Time: 2 ms (56.67%), Space: 5.9 MB (71.30%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -4,7 +4,7 @@ public:
         long ans=1;
         int mod=1e9+7;
         for(int i=1;i<=n;i++){
-            ans=ans*i*(2*i-1)%mod;
+            ans=ans*(2*i-1)*i%mod;
         }
         return ans;
     }
